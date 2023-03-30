a4a01b92664e6ab0d1a85c23d4fc2ffeeb8d2b67
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-30 11:41:33 -0700

    proto

--- main.h
+++ main.h
@@ -5,3 +5,21 @@
 #include <stddef.h>
 #include <stdarg.h>
 
+
+/**
+*struct format_t - A structure defining a type and its corresponding function
+*@type: The format specifier character for the argument type
+*@print_type: Pointer to the function that prints the argument type
+*/
+typedef struct format_t
+{
+	char type;
+	int (*print_type)(va_list args);
+} fmt_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_CHAR(va_list args);
+int print_STR(va_list args);
+int print_DEC(va_list args);
+int print_PER(va_list args);
